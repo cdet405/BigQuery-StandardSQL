48ddefc9cf124ff9c7b86632ac835393f1fef243
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-04-10 17:34:04 -0400

    Create hqOrdersVhqShipments.sql
    amount of orders incoming per day, vs the amount of orders being shipped out per day

---
+++ hqOrdersVhqShipments.sql
@@ -0,0 +1,98 @@
+-- orders vs shipments by day
+WITH csc AS(
+  SELECT  
+    company_id,
+    shipped_date,
+    COUNT(
+      DISTINCT 
+      m.order_number
+    ) distinctOrdersShipped,
+    COUNT(
+      DISTINCT 
+      s.number
+    ) distinctShipments
+  FROM `project.dataset.shipments` s, UNNEST(moves) m
+  WHERE warehouse_type = 'operated'
+  GROUP BY 
+    company_id, 
+    shipped_date
+  ORDER BY 
+    shipped_date DESC,
+    company_id ASC
+),
+cso AS(
+  SELECT 
+    company_id, 
+    FORMAT_DATETIME(
+      '%F', 
+      DATETIME(
+        TIMESTAMP(confirmation_time), 
+        "EST"
+      )
+    ) confirmation_date, 
+    COUNT(
+      DISTINCT 
+      order_number
+    ) ordersPlaced 
+  FROM 
+    `project.dataset.sales_orders` 
+    LEFT JOIN UNNEST(lines) l 
+  WHERE 
+    (
+      l.line_type = 'sale' 
+      OR l.line_type IS NULL
+    ) 
+    AND state NOT IN(
+      'draft', 
+      'cancel', 
+      'failed', 
+      'ignored'
+    ) 
+    AND order_date >= '2022-11-01' 
+    AND (
+      l.warehouse_name LIKE '%HQ%' 
+      AND product_code != 'Shipping'
+    ) 
+    AND (
+      l.quantity > 0 
+      OR l.quantity IS NULL
+    ) 
+    AND channel_name NOT LIKE '%Intercompany Transfer' 
+  GROUP BY 
+    company_id, 
+    confirmation_date 
+  ORDER BY 
+    confirmation_date DESC, 
+    company_id ASC
+) 
+SELECT 
+  CASE WHEN cso.company_id = 2 THEN 'name1' 
+       WHEN cso.company_id = 3 THEN 'name2' 
+       WHEN cso.company_id = 4 THEN 'name3' 
+       WHEN cso.company_id = 5 THEN 'name4' 
+       WHEN cso.company_id = 6 THEN 'name5' 
+    ELSE CONCAT(
+      'ERR:[company_id{', 
+      cso.company_id,
+      '}undefined]'
+    ) 
+  END company_name, 
+  cso.confirmation_date date, 
+  cso.ordersPlaced, 
+  IFNULL(
+    csc.distinctOrdersShipped,
+     0
+    ) distinctOrdersShipped, 
+  IFNULL(
+    csc.distinctShipments, 
+    0
+  ) distinctShipments 
+FROM 
+  cso 
+  LEFT JOIN 
+    csc 
+      ON csc.company_id = cso.company_id 
+        AND DATE(csc.shipped_date) = DATE(cso.confirmation_date) 
+ORDER BY 
+  date DESC, 
+  company_name ASC
