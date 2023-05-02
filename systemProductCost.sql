631cce67ecd7154da9f052d09b01d3ad4bf45509
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-02 15:58:26 -0400

    Update systemProductCost.sql
    added unpivot functionality to retain company_id where previously the record would be dropped

--- systemProductCost.sql
+++ systemProductCost.sql
@@ -1,5 +1,7 @@
-WITH p AS(
-  SELECT 
+--* version 2 *--
+-- fetch product data
+with p as(
+  select 
     code,
     purchase_uom_name p_uom,
     default_uom_name d_uom,
@@ -8,25 +10,62 @@ WITH p AS(
     c.cost,
     c.costing_method method,
     c.company_name,
+    c.company_id,
     c.currency_code
-  FROM `project.dataset.products`
-  ,UNNEST(costs) c 
-)
-SELECT 
-  * 
-FROM(
-  SELECT 
-    code, 
-    active,
-    p_uom,
-    d_uom,
-    s_uom, 
-    cost, 
-    company_id 
-  FROM p
-)
-PIVOT(
-  ANY_VALUE(cost) 
-    FOR company_id IN(1,2,3,4,5,6)
+  from `project.data.products`
+  ,unnest(costs) c 
+),
+-- piv p to retain all company variations
+piv as(
+  select 
+    * 
+  from(
+    select 
+      code, 
+      active,
+      p_uom,
+      d_uom,
+      s_uom, 
+      cost, 
+      company_id 
+    from p
+  )
+  pivot(
+    any_value(
+      cost
+    ) 
+    for company_id in(
+      1,2,3,4,5,6
     )
-ORDER BY code;
+  )
+order by code
+), 
+-- unpiv for future conditional joins
+unpiv as(
+  select 
+    * 
+  from piv 
+  unpivot(
+    cost for company_id in(
+      _1,_2,_3,_4,_5,_6
+    )
+  )
+),
+-- fix company_id from unpiv
+funpiv as(
+  select 
+    * except(company_id), 
+    cast(
+      right(
+        company_id,
+        1
+      ) 
+      as int64
+    ) company_id 
+  from unpiv 
+  order by code
+) 
+select 
+  * 
+from funpiv
+order by code
