e3f1fc118daf9a83079c3ea0c0d20aa6b43670e5
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-08 12:57:15 -0400

    Create spRefreshFVstg.sql

---
+++ spRefreshFVstg.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE PROCEDURE `PROJECT.DATASET.spRefreshFVstg`(tableName STRING)
+BEGIN 
+DECLARE deactivate, import, build, location STRING;
+SET location = CONCAT("`PROJECT.DATASET.",tableName,"`");
+
+SET deactivate = FORMAT(
+  """
+UPDATE `PROJECT.DATASET.forecastVelocityStg`
+SET active = false
+WHERE active = true; 
+  """
+);
+
+SET import = FORMAT(
+  """
+  INSERT INTO `PROJECT.DATASET.forecastVelocityStg`
+(
+  company_name,
+  product_code,
+  weekNum,
+  qty,
+  weekPeriod,
+  runDate,
+  active,
+  mqty,
+  confidencyLevel,
+  previousActual,
+  sid
+)
+SELECT 
+  'company' AS company_name, 
+  sku AS product_code, 
+  week AS weekNum, 
+  qty, 
+  weekPeriod, 
+  runDate, 
+  true AS active,
+  mqty,
+  cL AS confidencyLevel,
+  LTM26 AS previousActual,
+  sync_id AS sid
+FROM %s
+  """
+, location);
+
+SET build = FORMAT(
+  """
+  UPDATE `PROJECT.DATASET.forecastVelocity`
+  SET pd = NULL
+WHERE 1=1;
+CALL `PROJECT.DATASET.spForecastVelocity`();
+  """
+);
+
+EXECUTE IMMEDIATE deactivate;
+EXECUTE IMMEDIATE import;
+EXECUTE IMMEDIATE build;
+
+END;
