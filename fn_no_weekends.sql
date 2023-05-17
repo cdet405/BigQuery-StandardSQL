b7abf69b893f449b3b4c2e8a8cc1f51dbae22638
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-16 21:23:57 -0400

    Update fn_no_weekends.sql

--- fn_no_weekends.sql
+++ fn_no_weekends.sql
@@ -1,8 +1,11 @@
-CREATE OR REPLACE FUNCTION `project.dataset.fn_no_weekends`(dateValue DATE) 
+-- planned_date will sometimes assign to a weekend. this prevents that.
+-- will need to pull in assigned_at in addition to planned_date to hit friday night. 
+CREATE OR REPLACE FUNCTION `project.dataset.fn_no_weekends`(dateValue DATE, ts TIMESTAMP) 
 RETURNS DATE AS (
--- planned_date set by system doesnt respect user level date params
-  CASE WHEN FORMAT_DATE('%A',DATE(dateValue)) = 'Saturday' THEN DATE(dateValue + INTERVAL 2 DAY)
-       WHEN FORMAT_DATE('%A',DATE(dateValue)) = 'Sunday' THEN DATE(dateValue + INTERVAL 1 DAY)
+  CASE WHEN FORMAT_DATE('%A',DATE(dateValue)) = 'Saturday' THEN DATE(dateValue + INTERVAL 3 DAY)
+       WHEN FORMAT_DATE('%A',DATE(dateValue)) = 'Sunday' THEN DATE(dateValue + INTERVAL 2 DAY)
+       WHEN (FORMAT_DATE('%A',DATE(dateValue)) = 'Friday' AND CAST(FORMAT_DATETIME('%k',DATETIME(TIMESTAMP(ts), 'America/New_York')) AS INT64) >= 15)THEN DATE(dateValue + INTERVAL 3 DAY)
        ELSE DATE(dateValue)
   END
 );
+ 
