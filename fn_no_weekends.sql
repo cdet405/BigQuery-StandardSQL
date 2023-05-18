a261263203db529641a1377dc60c19dccdaafd1d
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-18 14:16:17 -0400

    Update fn_no_weekends.sql
    added logic for assigned after cutoff

--- fn_no_weekends.sql
+++ fn_no_weekends.sql
@@ -1,11 +1,94 @@
 -- planned_date will sometimes assign to a weekend. this prevents that.
--- will need to pull in assigned_at in addition to planned_date to hit friday night. 
-CREATE OR REPLACE FUNCTION `project.dataset.fn_no_weekends`(dateValue DATE, ts TIMESTAMP) 
+-- as well as pushing planned_date +1 day when assigned after cutoff
+CREATE OR REPLACE FUNCTION `project.dataset.fn_no_weekends`
+(
+  dateValue DATE,
+  ts TIMESTAMP
+) 
 RETURNS DATE AS (
-  CASE WHEN FORMAT_DATE('%A',DATE(dateValue)) = 'Saturday' THEN DATE(dateValue + INTERVAL 3 DAY)
-       WHEN FORMAT_DATE('%A',DATE(dateValue)) = 'Sunday' THEN DATE(dateValue + INTERVAL 2 DAY)
-       WHEN (FORMAT_DATE('%A',DATE(dateValue)) = 'Friday' AND CAST(FORMAT_DATETIME('%k',DATETIME(TIMESTAMP(ts), 'America/New_York')) AS INT64) >= 15)THEN DATE(dateValue + INTERVAL 3 DAY)
-       ELSE DATE(dateValue)
+  CASE 
+    WHEN 
+      FORMAT_DATE(
+        '%A',
+        DATE(
+          dateValue
+        )
+      ) = 'Saturday' 
+        THEN 
+          DATE(
+            dateValue + INTERVAL 3 DAY
+          )
+    WHEN 
+      FORMAT_DATE(
+        '%A',
+        DATE(
+          dateValue
+        )
+      ) = 'Sunday' 
+        THEN 
+          DATE(
+            dateValue + INTERVAL 2 DAY
+          )
+    WHEN 
+      (
+        FORMAT_DATE(
+          '%A',
+          DATE(
+            dateValue
+          )
+        ) = 'Friday' 
+        AND 
+        CAST(
+          FORMAT_DATETIME(
+            '%k',
+            DATETIME(
+              TIMESTAMP(
+                ts
+              ),
+              'America/New_York'
+            )
+          ) AS INT64
+        ) 
+        >= 14
+      )
+        THEN 
+          DATE(
+            dateValue + INTERVAL 3 DAY
+          )
+      WHEN 
+        (
+          (
+            DATE(
+              dateValue
+            ) 
+            = 
+            DATE(
+              ts
+            )
+          ) 
+          AND 
+          CAST(
+            FORMAT_DATETIME(
+              '%k',
+              DATETIME(
+                TIMESTAMP(
+                  ts
+                ), 
+                'America/New_York'
+              )
+            ) 
+            AS INT64
+          ) 
+          >= 14
+        )
+          THEN 
+            DATE(
+              dateValue + INTERVAL 1 DAY
+            )
+      ELSE 
+        DATE(
+          dateValue
+        )
   END
 );
- 
+       
