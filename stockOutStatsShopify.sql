041a17e9d961d82658a43d7960420a66ad952dad
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-18 16:18:57 -0400

    Create stockOutStatsShopify.sql

---
+++ stockOutStatsShopify.sql
@@ -0,0 +1,143 @@
+-- shopify stock out stats
+with ss as(
+  select 
+    vid, 
+    sku,
+    runDate,
+    price, 
+    available 
+  from `project.datsset.shopifyScrape`
+  where site not like 's%s'
+),
+-- average retail
+ar as(
+  select
+    vid,
+    round(avg(price),2) avgRetail
+  from ss 
+  group by 1
+),
+-- last listed
+ll as(
+  select
+  vid,
+  max(runDate) lastListDate,
+  from ss
+  group by 1
+),
+-- distinct vid 
+dv as(
+  select 
+   distinct
+    vid,
+    sku
+  from ss
+  -- removes records prior to mapping
+  where sku is not null
+),
+-- lag dates
+lg as(
+  select
+    vid,
+    runDate,
+    available,
+    ifnull(
+      lag(
+        runDate,
+        1
+      ) over (
+        PARTITION BY vid 
+        order by runDate desc
+      ),
+      ll.lastListDate
+    ) nxt
+from ss
+left join ll using (vid)
+where available = true
+),
+-- adds datediffs
+d as(
+  select 
+    *, 
+    date_diff(
+      nxt,
+      runDate,
+      DAY
+    ) dd 
+  from lg
+),
+-- bring in sales
+s as(
+  select 
+    company_id,
+    order_date,
+    channel_name,
+    product_code,
+    unit_price,
+    quantity
+  from `fulfil-data-warehouse-227710.holdings_365.sales_orders` s
+  ,unnest(lines) l   
+  where order_date >= '2022-12-15'
+  and l.line_type='sale' and s.state in('processing','done')
+),
+-- group sales
+sg as(
+  select
+    product_code,
+    order_date,
+    sum(quantity) tot
+  from s
+  group by 1,2
+),
+pre as(
+  select 
+    dv.sku,  
+    d.*,
+    ar.avgRetail 
+  from d 
+  join dv using (vid) 
+  join ar using (vid)
+  where ifnull(dd,0)>1 
+  order by 1,3 asc
+),
+mid as (
+  select 
+    pre.*,
+    round(
+      safe_divide(
+        (
+          select
+            sum(tot)
+          from sg
+          where pre.sku=sg.product_code
+          and(
+            sg.order_date <= pre.runDate
+            and sg.order_date >= pre.runDate - interval 14 day
+          )
+        ),
+       14
+      ),
+    2
+    ) prev14DA,
+    round(
+      safe_divide(
+        pre.dd,
+        14
+      ),
+     14
+    ) Lratio
+  from pre
+) 
+select
+ *,
+ round(
+   (
+     (
+       dd*prev14DA
+      )-- *Lratio #removed (already ratio'd bc its a DA)
+    )*avgRetail,
+   2
+ ) missedRev
+from mid 
+order by 2,3
+
