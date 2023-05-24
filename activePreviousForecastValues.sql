a729188932f5f944e7bf1de033aa943d87301ab4
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-05-24 13:25:39 -0400

    Create activePreviousForecastValues.sql

---
+++ activePreviousForecastValues.sql
@@ -0,0 +1,45 @@
+-- filters log table to only records that were actually active on a given date
+-- data source for forecast accuracy scorecard
+WITH fh AS(
+  SELECT
+    *, 
+    row_number() 
+      OVER (
+        PARTITION BY 
+          product_code,
+          weekNum 
+        ORDER BY 
+          runDate
+      ) revNum
+  FROM `project.dataset.forecastVelocityStg`
+)
+-- Final Forecast History Data Set
+SELECT
+  fh.product_code,
+  fh.weekNum,
+  fh.qty,
+  fh.weekPeriod,
+  fh.revNum,
+  fh.runDate 
+FROM fh
+ -- This Join Filters Data Set forecast numbers by latest upload per product per weekNum
+ -- aka where the magic happens
+ JOIN(
+   SELECT
+     product_code,
+     weekNum,
+     MAX(revNum) lastRev
+   FROM fh
+  GROUP BY 
+    product_code, 
+    weekNum
+ ) mfh 
+     ON (
+     fh.product_code = mfh.product_code 
+     AND fh.weekNum = mfh.weekNum 
+     AND fh.revNum = mfh.lastRev
+     )
+ORDER BY  
+  product_code, 
+  weekNum, 
+  revNum 
