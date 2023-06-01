19d5ad73616240e3ca85c8585cfdc9ce55a8ae52
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-06-01 14:56:34 -0400

    Create gorgSurveyMerge.sql

---
+++ gorgSurveyMerge.sql
@@ -0,0 +1,56 @@
+-- merges staging table to production table
+-- will be used in the connector script
+merge gorgSurvey p
+using (
+	select 
+	  * 
+	from gorgSurvey 
+	where load_date = current_date()
+) s
+on (
+	p.id=s.id and 
+	p.customer_id = s.customer_id
+)
+when matched then
+update set
+	p.ticket_id = s.ticket_id,
+	p.sent_datetime = s.sent_datetime,
+	p.scored_datetime = s.scored_datetime,
+	p.meta = s.meta,
+	p.score = s.score,
+	p.customer_id = s.customer_id,
+	p.created_datetime = s.created_datetime,
+	p.uri = s.uri,
+	p.body_text = s.body_text,
+	p.should_send_datetime = s.should_send_datetime,
+	p.id = s.id
+	--p.load_date = s.load_date -- these will never match potentially causing wasted resource on updating ts if nothing truely changed 
+when not matched then
+  insert(
+	  ticket_id,
+	  sent_datetime,
+	  scored_datetime,
+	  meta,
+	  score,
+	  customer_id,
+	  created_datetime,
+	  uri,
+	  body_text,
+	  should_send_datetime,
+	  id,
+	  load_date
+)
+values(
+  s.ticket_id,
+  s.sent_datetime,
+  s.scored_datetime,
+  s.meta,
+  s.score,
+  s.customer_id,
+  s.created_datetime,
+  s.uri,
+  s.body_text,
+  s.should_send_datetime,
+  s.id,
+  s.load_date
+);
