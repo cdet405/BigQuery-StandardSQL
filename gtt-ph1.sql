e7f0e6b7e38ed527ad72cf74b8dcd400f7fd1f1d
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-06-06 15:45:51 -0400

    Create gtt-ph1.sql
    ndjson transfer breaks on multiple columns/arrays. trying to import via the ugliest csv ever in the mean time to hit sla. 
    sorry in advance for the json column 😿 
    
    table schema phase one gtt.
    current plan gttph1>gttstg>gtt

---
+++ gtt-ph1.sql
@@ -0,0 +1,45 @@
+-- phase one ticket ingest 
+-- theres gotta be a better way
+-- //TODO: figure out a better way to do this
+
+create or replace table manifest.gtt1(
+  opened_datetime timestamp,
+  priority string,
+  subject string,	
+  created_datetime timestamp,
+  uri string,	
+  assignee_user_lastname string,	
+  assignee_user_id int64,	
+  assignee_team string,	
+  integrations json options(description="you either die a hero, or live long enough to become the villian"),	
+  assignee_user_email string,	
+  customer_firstname string,	
+  last_received_message_datetime timestamp,	
+  channel string,	
+  snooze_datetime timestamp,	
+  customer_lastname timestamp,	
+  from_agent bool,	
+  spam bool,	
+  messages_count int64,	
+  last_message_datetime timestamp,	
+  closed_datetime timestamp,	
+  id int64 not null,	
+  updated_datetime timestamp,	
+  customer_id int64,	
+  customer_name string,	
+  assignee_user_firstname string,	
+  assignee_user string,	
+  external_id int64,	
+  is_unread bool,	
+  trashed_datetime timestamp,	
+  excerpt string,	
+  assignee_user_name string,	
+  tags json options(description="im sorry for this, im in my villian arc"),	
+  language string,	
+  status string,	
+  customer_email string,	
+  via string,
+  load_ts timestamp default current_timestamp() options(description="ts inserted by user/script"),
+  mod_ts timestamp default null options(description="ts row modifified (prob wont be used)")
+)
+options(description=("gtt-ph1-transform"))
