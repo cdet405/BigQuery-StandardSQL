4e3358815e66590f32223c4e35b6b98ba375dd4a
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-06-13 15:13:48 -0400

    Update gorgTicketMerge.sql
    insert row caused conflicts.
    modified for prod

--- gorgTicketMerge.sql
+++ gorgTicketMerge.sql
@@ -1,13 +1,13 @@
 -- merge statement to be used for ticket pipeline
--- dev version 1 - for testing + concept 23613CD
+-- prod version 1.1 - 20230613CD
 ---------------------------------------------------
-merge manifest.pdtest3 p -- //todo: update table names to prod & stg
-using manifest.pdtest2 s -- //todo: update table names to prod & stg
+merge manifest.gorgTicket p 
+using manifest.gorgTicketStg s 
 on p.id = s.id 
 when 
   matched 
     and(
-      p.updated_datetime != s.updated_datetime -- //todo: this should be the only column needed for update criteria (nned to verify)
+      p.updated_datetime != s.updated_datetime
     ) 
   then 
     update
@@ -24,6 +24,7 @@ when
         p.subject = s.subject,
         p.tags = s.tags,
         p.is_unread = s.is_unread,
+        p.spam = s.spam,
         p.opened_datetime = s.opened_datetime,
         p.last_received_message_datetime = s.last_received_message_datetime,
         p.last_message_datetime = s.last_message_datetime,
@@ -32,10 +33,69 @@ when
         p.snooze_datetime = s.snooze_datetime,
         p.trashed_datetime = s.trashed_datetime,
         p.integrations = s.integrations,
-        p.messages_count = s.messages_count
-        -- ,p.mod_ts = current_date() -- //todo: add mod_ts column to prod table & uncomment 
+        p.messages_count = s.messages_count,
+        p.mod_ts = current_datetime('America/New_York')
 when 
   not matched 
     then 
-      insert row
-      -- //todo: add load_ts to prod/stg table with default value of current_date()
+      insert(
+        id,
+        uri,
+        external_id,
+        language,
+        status,
+        priority,
+        channel,
+        via,
+        customer,
+        assignee_user,
+        assignee_team,
+        subject,
+        tags,
+        is_unread,
+        spam,
+        created_datetime,
+        opened_datetime,
+        last_received_message_datetime,
+        last_message_datetime,
+        updated_datetime,
+        closed_datetime,
+        snooze_datetime,
+        trashed_datetime,
+        integrations,
+        messages_count,
+        load_ts,
+        mod_ts,
+        from_agent
+      )
+      values(
+        s.id,
+        s.uri,
+        s.external_id,
+        s.language,
+        s.status,
+        s.priority,
+        s.channel,
+        s.via,
+        s.customer,
+        s.assignee_user,
+        s.assignee_team,
+        s.subject,
+        s.tags,
+        s.is_unread,
+        s.spam,
+        s.created_datetime,
+        s.opened_datetime,
+        s.last_received_message_datetime,
+        s.last_message_datetime,
+        s.updated_datetime,
+        s.closed_datetime,
+        s.snooze_datetime,
+        s.trashed_datetime,
+        s.integrations,
+        s.messages_count,
+        s.load_ts,
+        s.mod_ts,
+        s.from_agent
+      )
+;
