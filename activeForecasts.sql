bf6dab5311ffc1036f1845a07000f6a4b4fa7a41
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-06-16 15:34:35 -0400

    Create activeForecasts.sql

---
+++ activeForecasts.sql
@@ -0,0 +1,16 @@
+-- fetch all active forecasts in erp
+select 
+  current_datetime('America/New_York') runDate,
+  name,
+  state,
+  start_date,
+  end_date,
+  create_user_name,
+  write_user_name,
+  warehouse_name,
+  l.*
+from `project.dataset.inventory_forecasts`
+,unnest(lines) l
+where state = 'confirmed'
+  and current_date() between start_date and end_date
+;
