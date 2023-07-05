8496fb4551e312b38c391ca95dcc79a43f69cb20
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-07-05 13:55:28 -0400

    Update ccicpiddl.sql
    column calc was off, fixed values

--- ccicpiddl.sql
+++ ccicpiddl.sql
@@ -19,3 +19,24 @@ from files(
   skip_leading_rows = 1,
   uris = ['gs://bucket/folder/ccicpibase.csv']
 );
+-- create working table 
+create table manifest.tmpFix(
+  x float64,
+  y int64
+);
+-- load data for update 
+load data into manifest.tmpFix
+from files(
+  format = 'CSV',
+  skip_leading_rows = 1,
+  uris = ['gs://bucket/folder/fix.csv']
+);
+-- set fixed values 
+merge manifest.ccicpi p
+using manifest.tmpFix s
+on p.sid = s.y
+when matched 
+then update
+set p.cpi_mm = s.x;
+-- clean up 
+drop table if exists manifest.tmpFix;
