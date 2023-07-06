f959723bb2fceb6218ec37e1b92e1a41c53cf326
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-07-06 07:17:13 -0400

    Update fn_no_weekends.sql
    Added a hard override (used for holiday) its a jank, bandaid solution, working on creating a better alternative.

--- fn_no_weekends.sql
+++ fn_no_weekends.sql
@@ -1,94 +1,185 @@
 -- planned_date will sometimes assign to a weekend. this prevents that.
 -- as well as pushing planned_date +1 day when assigned after cutoff
-CREATE OR REPLACE FUNCTION `project.dataset.fn_no_weekends`
-(
-  dateValue DATE,
+-- added single day override (bandaid while working on a solution for holidays) 
+CREATE OR REPLACE FUNCTION manifest.fn_no_weekends(
+  dateValue DATE, 
   ts TIMESTAMP
-) 
+)
 RETURNS DATE AS (
-  CASE 
-    WHEN 
-      FORMAT_DATE(
-        '%A',
-        DATE(
-          dateValue
-        )
-      ) = 'Saturday' 
-        THEN 
-          DATE(
-            dateValue + INTERVAL 3 DAY
-          )
-    WHEN 
-      FORMAT_DATE(
-        '%A',
-        DATE(
-          dateValue
-        )
-      ) = 'Sunday' 
-        THEN 
-          DATE(
-            dateValue + INTERVAL 2 DAY
-          )
-    WHEN 
-      (
-        FORMAT_DATE(
-          '%A',
-          DATE(
-            dateValue
-          )
-        ) = 'Friday' 
-        AND 
-        CAST(
-          FORMAT_DATETIME(
-            '%k',
-            DATETIME(
-              TIMESTAMP(
-                ts
-              ),
-              'America/New_York'
-            )
-          ) AS INT64
-        ) 
-        >= 14
-      )
-        THEN 
-          DATE(
-            dateValue + INTERVAL 3 DAY
-          )
-      WHEN 
-        (
-          (
+  IF(
+    (
+      CASE 
+        WHEN 
+          FORMAT_DATE(
+            '%A',
             DATE(
               dateValue
-            ) 
-            = 
-            DATE(
-              ts
             )
-          ) 
-          AND 
-          CAST(
-            FORMAT_DATETIME(
-              '%k',
-              DATETIME(
-                TIMESTAMP(
-                  ts
-                ), 
-                'America/New_York'
+          ) = 'Saturday' 
+            THEN 
+              DATE(
+                dateValue + INTERVAL 3 DAY
               )
-            ) 
-            AS INT64
-          ) 
-          >= 14
-        )
-          THEN 
+        WHEN 
+          FORMAT_DATE(
+            '%A',
             DATE(
-              dateValue + INTERVAL 1 DAY
+              dateValue
             )
-      ELSE 
-        DATE(
-          dateValue
-        )
-  END
-);
-       
+          ) = 'Sunday' 
+            THEN 
+              DATE(
+                dateValue + INTERVAL 2 DAY
+              )
+        WHEN 
+          (
+            FORMAT_DATE(
+              '%A',
+              DATE(
+                dateValue
+              )
+            ) = 'Friday' 
+            AND 
+            CAST(
+              FORMAT_DATETIME(
+                '%k',
+                DATETIME(
+                  TIMESTAMP(
+                    ts
+                  ),
+                  'America/New_York'
+                )
+              ) AS INT64
+            ) 
+            >= 14
+          )
+            THEN 
+              DATE(
+                dateValue + INTERVAL 3 DAY
+              )
+          WHEN 
+            (
+              (
+                DATE(
+                  dateValue
+                ) 
+                = 
+                DATE(
+                  ts
+                )
+              ) 
+              AND 
+              CAST(
+                FORMAT_DATETIME(
+                  '%k',
+                  DATETIME(
+                    TIMESTAMP(
+                      ts
+                    ), 
+                    'America/New_York'
+                  )
+                ) 
+                AS INT64
+              ) 
+              >= 14
+            )
+              THEN 
+                DATE(
+                  dateValue + INTERVAL 1 DAY
+                )
+          ELSE 
+            DATE(
+              dateValue
+            )
+       END
+    )
+    = '2023-07-04', -- temporary fix / override date
+    '2023-07-06', -- temporary fix / replacement date 
+        (
+      CASE 
+        WHEN 
+          FORMAT_DATE(
+            '%A',
+            DATE(
+              dateValue
+            )
+          ) = 'Saturday' 
+            THEN 
+              DATE(
+                dateValue + INTERVAL 3 DAY
+              )
+        WHEN 
+          FORMAT_DATE(
+            '%A',
+            DATE(
+              dateValue
+            )
+          ) = 'Sunday' 
+            THEN 
+              DATE(
+                dateValue + INTERVAL 2 DAY
+              )
+        WHEN 
+          (
+            FORMAT_DATE(
+              '%A',
+              DATE(
+                dateValue
+              )
+            ) = 'Friday' 
+            AND 
+            CAST(
+              FORMAT_DATETIME(
+                '%k',
+                DATETIME(
+                  TIMESTAMP(
+                    ts
+                  ),
+                  'America/New_York'
+                )
+              ) AS INT64
+            ) 
+            >= 14
+          )
+            THEN 
+              DATE(
+                dateValue + INTERVAL 3 DAY
+              )
+          WHEN 
+            (
+              (
+                DATE(
+                  dateValue
+                ) 
+                = 
+                DATE(
+                  ts
+                )
+              ) 
+              AND 
+              CAST(
+                FORMAT_DATETIME(
+                  '%k',
+                  DATETIME(
+                    TIMESTAMP(
+                      ts
+                    ), 
+                    'America/New_York'
+                  )
+                ) 
+                AS INT64
+              ) 
+              >= 14
+            )
+              THEN 
+                DATE(
+                  dateValue + INTERVAL 1 DAY
+                )
+          ELSE 
+            DATE(
+              dateValue
+            )
+      END
+    )
+  )
+);  
