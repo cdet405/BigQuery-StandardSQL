117d8f6454b8b5835826d71a87852e234d50085a
cdet405 <58343544+cdet405@users.noreply.github.com>
2023-07-06 12:46:59 -0400

    Update fn_no_weekends.sql
    removed the bandaid override fix, added reference table for ease of adding  restricted dates.

--- fn_no_weekends.sql
+++ fn_no_weekends.sql
@@ -1,185 +1,115 @@
 -- planned_date will sometimes assign to a weekend. this prevents that.
 -- as well as pushing planned_date +1 day when assigned after cutoff
--- added single day override (bandaid while working on a solution for holidays) 
+-- added calendar reference table for restricted days i.e holidays/nonworking
 CREATE OR REPLACE FUNCTION manifest.fn_no_weekends(
   dateValue DATE, 
   ts TIMESTAMP
-)
-RETURNS DATE AS (
-  IF(
-    (
-      CASE 
-        WHEN 
-          FORMAT_DATE(
-            '%A',
-            DATE(
-              dateValue
-            )
-          ) = 'Saturday' 
-            THEN 
-              DATE(
-                dateValue + INTERVAL 3 DAY
-              )
-        WHEN 
-          FORMAT_DATE(
-            '%A',
-            DATE(
-              dateValue
-            )
-          ) = 'Sunday' 
-            THEN 
-              DATE(
-                dateValue + INTERVAL 2 DAY
-              )
-        WHEN 
-          (
-            FORMAT_DATE(
-              '%A',
+) RETURNS DATE AS (
+  (
+    WITH x AS(
+      SELECT
+        dates,
+        resume_date
+      FROM manifest.calendar
+    ),
+    d AS(
+      SELECT
+        CASE
+            WHEN 
+              FORMAT_DATE(
+                '%A', 
+                DATE(
+                  dateValue
+                )
+              ) = 'Saturday' 
+                THEN 
+                  DATE(
+                    dateValue 
+                    + INTERVAL 3 DAY
+                  )
+            WHEN 
+              FORMAT_DATE(
+                '%A', 
+                DATE(
+                  dateValue
+                )
+              ) = 'Sunday' 
+                THEN 
+                  DATE(
+                    dateValue 
+                    + INTERVAL 2 DAY
+                 )
+            WHEN
+              (
+                FORMAT_DATE(
+                  '%A', 
+                  DATE(
+                    dateValue
+                  )
+                ) = 'Friday' 
+                AND 
+                  CAST(
+                    FORMAT_DATETIME(
+                      '%k', 
+                      DATETIME(
+                        TIMESTAMP(
+                          ts
+                        ), 
+                       'America/New_York'
+                      )
+                   ) 
+                   AS INT64
+                 ) 
+                >= 14
+              ) 
+              THEN 
+                DATE(
+                  dateValue 
+                  + INTERVAL 3 DAY
+                )
+            WHEN 
+              (
+                (
+                  DATE(
+                    dateValue
+                  ) 
+                  = 
+                  DATE(
+                    ts
+                  )
+               ) 
+                AND 
+                  CAST(
+                    FORMAT_DATETIME(
+                      '%k', 
+                      DATETIME(
+                        TIMESTAMP(
+                          ts
+                        ), 
+                       'America/New_York'
+                      )
+                    ) 
+                   AS INT64
+                  ) 
+                >= 14
+              ) 
+              THEN 
+                DATE(
+                  dateValue 
+                  + INTERVAL 1 DAY
+                )
+            ELSE 
               DATE(
                 dateValue
               )
-            ) = 'Friday' 
-            AND 
-            CAST(
-              FORMAT_DATETIME(
-                '%k',
-                DATETIME(
-                  TIMESTAMP(
-                    ts
-                  ),
-                  'America/New_York'
-                )
-              ) AS INT64
-            ) 
-            >= 14
-          )
-            THEN 
-              DATE(
-                dateValue + INTERVAL 3 DAY
-              )
-          WHEN 
-            (
-              (
-                DATE(
-                  dateValue
-                ) 
-                = 
-                DATE(
-                  ts
-                )
-              ) 
-              AND 
-              CAST(
-                FORMAT_DATETIME(
-                  '%k',
-                  DATETIME(
-                    TIMESTAMP(
-                      ts
-                    ), 
-                    'America/New_York'
-                  )
-                ) 
-                AS INT64
-              ) 
-              >= 14
-            )
-              THEN 
-                DATE(
-                  dateValue + INTERVAL 1 DAY
-                )
-          ELSE 
-            DATE(
-              dateValue
-            )
-       END
+      END output
     )
-    = '2023-07-04', -- temporary fix / override date
-    '2023-07-06', -- temporary fix / replacement date 
-        (
-      CASE 
-        WHEN 
-          FORMAT_DATE(
-            '%A',
-            DATE(
-              dateValue
-            )
-          ) = 'Saturday' 
-            THEN 
-              DATE(
-                dateValue + INTERVAL 3 DAY
-              )
-        WHEN 
-          FORMAT_DATE(
-            '%A',
-            DATE(
-              dateValue
-            )
-          ) = 'Sunday' 
-            THEN 
-              DATE(
-                dateValue + INTERVAL 2 DAY
-              )
-        WHEN 
-          (
-            FORMAT_DATE(
-              '%A',
-              DATE(
-                dateValue
-              )
-            ) = 'Friday' 
-            AND 
-            CAST(
-              FORMAT_DATETIME(
-                '%k',
-                DATETIME(
-                  TIMESTAMP(
-                    ts
-                  ),
-                  'America/New_York'
-                )
-              ) AS INT64
-            ) 
-            >= 14
-          )
-            THEN 
-              DATE(
-                dateValue + INTERVAL 3 DAY
-              )
-          WHEN 
-            (
-              (
-                DATE(
-                  dateValue
-                ) 
-                = 
-                DATE(
-                  ts
-                )
-              ) 
-              AND 
-              CAST(
-                FORMAT_DATETIME(
-                  '%k',
-                  DATETIME(
-                    TIMESTAMP(
-                      ts
-                    ), 
-                    'America/New_York'
-                  )
-                ) 
-                AS INT64
-              ) 
-              >= 14
-            )
-              THEN 
-                DATE(
-                  dateValue + INTERVAL 1 DAY
-                )
-          ELSE 
-            DATE(
-              dateValue
-            )
-      END
-    )
+    SELECT
+      COALESCE(
+        resume_date,
+        output
+      ) AS dateValue
+    FROM d
+    INNER JOIN x ON x.dates = d.output
   )
-);  
+);
